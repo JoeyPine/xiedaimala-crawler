3cdf3f894a3363b46877b71204ebd33c865b0363
Joeypine <fanjiasong_7@outlook.com>
2020-05-08 20:42:21 +0800

    引入MyBatis简化数据库操作

--- src/main/java/com/github/hcsp/JdbcCrawlerDao.java
+++ src/main/java/com/github/hcsp/JdbcCrawlerDao.java
@@ -19,7 +19,7 @@ public class JdbcCrawlerDao implements CrawlerDao{
     }
 
 
-    public String getNextLink(String sql) throws SQLException {
+    private String getNextLink(String sql) throws SQLException {
         ResultSet resultSet = null;
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
             resultSet = statement.executeQuery();
@@ -73,5 +73,15 @@ public class JdbcCrawlerDao implements CrawlerDao{
             }
         }
         return false;
+    }
+
+    @Override
+    public void insertProcessesLink(String link) {
+
+    }
+
+    @Override
+    public void insertLinkToBeProcessed(String href) {
+
     }
 }
